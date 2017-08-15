117f2472eb33292d2a6b3113ea79694ed5d9d6a8
Hristijan Lubeski <htlubeski@gmail.com>
2017-08-15 16:44:42 +0200

    Adds db session as well as file indexing

--- db/repository.go
+++ db/repository.go
@@ -4,6 +4,7 @@ import (
 	"github.com/JormungandrK/microservice-user-profile/app"
 )
 
+
 // UserProfileRepository defaines the interface for accessing the user profile data
 type UserProfileRepository interface {
 	// GetUserProfile looks up a UserProfile by the user ID. Returns app.UserProfile if found or nil if not found.
@@ -13,4 +14,53 @@ type UserProfileRepository interface {
 	// If the profile already exists, it updates the data. If not profile entry exists, a new one is created.
 	// Returns the updated or newly created user profile.
 	UpdateUserProfile(profile app.UserProfilePayload) (*app.UserProfile, error)
+}
+
+// MongoCollection wraps a mgo.Collection to embed methods in models.
+type MongoCollection struct {
+	*mgo.Collection
+}
+
+// NewSession returns a new Mongo Session.
+func NewSession(Host string, Username string, Password string, Database string) *mgo.Session {
+	session, err := mgo.DialWithInfo(&mgo.DialInfo{
+		Addrs:    []string{Host},
+		Username: Username,
+		Password: Password,
+		Database: Database,
+		Timeout:  30 * time.Second,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	// SetMode - consistency mode for the session.
+	session.SetMode(mgo.Monotonic, true)
+
+	return session
+}
+
+// PrepareDB ensure presence of persistent and immutable data in the DB.
+func PrepareDB(session *mgo.Session, db string, dbCollection string, indexes []string) *mgo.Collection {
+	// Create collection
+	collection := session.DB(db).C(dbCollection)
+
+	// Define indexes
+	for _, elem := range indexes {
+		i := []string{elem}
+		index := mgo.Index{
+			Key:        i,
+			Unique:     true,
+			DropDups:   true,
+			Background: true,
+			Sparse:     true,
+		}
+
+		// Create indexes
+		if err := collection.EnsureIndex(index); err != nil {
+			panic(err)
+		}
+	}
+
+	return collection
 }
--- main.go
+++ main.go
@@ -10,6 +10,14 @@ import (
 	"github.com/JormungandrK/microservice-user-profile/app"
 	"github.com/goadesign/goa"
 	"github.com/goadesign/goa/middleware"
+	"gopkg.in/mgo.v2"
+)
+
+const (
+ 	Host     = "127.0.0.1:27017"
+ 	Username = "restapi"
+	Password = "restapi"
+	Database = "user-profile"
 )
 
 func main() {
@@ -17,20 +25,31 @@ func main() {
 	unregisterService := registerMicroservice()
 	defer unregisterService() // defer the unregister for after main exits
 
-	// Create service
-	service := goa.New("user-profile")
-
 	// Mount middleware
 	service.Use(middleware.RequestID())
 	service.Use(middleware.LogRequest(true))
 	service.Use(middleware.ErrorHandler(service, true))
 	service.Use(middleware.Recover())
 
+	service := goa.New("user-profile")
+
+	// Load MongoDB ENV variables
+	host, username, password, database := loadMongnoSettings()
+	// Create new session to MongoDB
+	session := store.NewSession(host, username, password, database)
+
+	// At the end close session
+	defer session.Close()
+
+	// Create users collection and indexes
+	indexes := []string{"fullname", "email"}
+	userProfileCollection := store.PrepareDB(session, database, "user-profiles", indexes)
+
 	// Mount "swagger" controller
 	c := NewSwaggerController(service)
 	app.MountSwaggerController(service, c)
 	// Mount "userProfile" controller
-	c2 := NewUserProfileController(service)
+	c2 := NewUserProfileController(service, &db.UserProfileRepository{Collection: userProfileCollection})
 	app.MountUserProfileController(service, c2)
 
 	// Start service
@@ -38,6 +57,28 @@ func main() {
 		service.LogError("startup", "err", err)
 	}
 
+}
+
+func loadMongnoSettings() (string, string, string, string) {
+	host := os.Getenv("MONGO_URL")
+	username := os.Getenv("MS_USERNAME")
+	password := os.Getenv("MS_PASSWORD")
+	database := os.Getenv("MS_DBNAME")
+
+	if host == "" {
+		host = "127.0.0.1:27017"
+	}
+	if username == "" {
+		username = "restapi"
+	}
+	if password == "" {
+		password = "restapi"
+	}
+	if database == "" {
+		database = "user-profile"
+	}
+
+	return host, username, password, database
 }
 
 func loadGatewaySettings() (string, string) {
